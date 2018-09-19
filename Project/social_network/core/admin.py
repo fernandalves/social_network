820d6fbd16c3d2d76bbf84b6fdead526e43a053e
Joao-Antunes <43221968+Joao-Antunes@users.noreply.github.com>
2018-09-19 12:12:16 -0300

    Add files via upload
    adicionado classes ao model e ao admin.

--- Project/social_network/core/admin.py
+++ Project/social_network/core/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
+from .models import Publicacao, Categoria
 
-# Register your models here.
+@admin.register(Categoria)
+class Categoria_Admin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {
+            'fields': ('tipo',)
+        }),
+    )
+
+@admin.register(Publicacao)
+class Publicacao_Admin(admin.ModelAdmin):
+    list_display = ('usuario', 'mensagem', 'dt_criacao', 'categoria',)
+    search_fields = ('mensagem', 'usuario__email', 'categoria' )
+    list_filter = ('usuario', 'categoria', )
+
+    fieldsets = (
+        (None, {
+            'fields': ('mensagem', )
+        }),
+        ('Categoria', {
+            'fields': ('categoria',)
+        }),
+        ("Dados", {
+            'fields': (('dt_criacao', 'dt_atualizacao', ), )
+        }),
+    )
+    readonly_fields = ('dt_criacao', 'dt_atualizacao', 'slug',)
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.usuario = request.user
+        super().save_model(request, obj, form, change)
--- Project/social_network/core/models.py
+++ Project/social_network/core/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Categoria(models.Model):
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ('tipo',)
+
+    tipo = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.tipo
+
+class Publicacao(models.Model):
+    class Meta:
+        verbose_name = "Publicação"
+        verbose_name_plural = "Publicações"
+        ordering = ('-dt_criacao', )
+
+    CATEGORIAS_CHOICES = (
+    )
+
+    usuario = models.ForeignKey(User, blank=True, on_delete=models.CASCADE)
+    mensagem = models.CharField(max_length=155, verbose_name="Mensagem", help_text="Digite uma mensagem")
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE, choices=CATEGORIAS_CHOICES)
+
+    dt_criacao = models.DateTimeField(verbose_name="Criado em", auto_now_add=True)
+    dt_atualizacao = models.DateTimeField(verbose_name="Atualizado em", auto_now=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return "%s: %s" % (self.usuario, self.mensagem)
